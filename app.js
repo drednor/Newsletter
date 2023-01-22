07a0fc7b2e4c2bc44e057d8b1fee243d671e739f
Abhishek Singh <singh.msoe@gmail.com>
2023-01-22 09:58:59 +0530

    added port number

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post("/failure", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("app is listening on port 3000");
 });
 // api key
